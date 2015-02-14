1ac0ffd713c016c54cd8b821e787e6adfe956858
driss78210 <drisskaaouas@gmail.com>
2015-02-15 00:42:41 +0100

    Modification de la classe Atraining dans laquelle des changements doivent être apportée. Ajouts des @Id @Column pour le fonctionnement d'hibernate.

--- src/model/ATraining.java
+++ src/model/ATraining.java
@@ -2,12 +2,23 @@ package model;
 
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.*;
 
 public abstract class ATraining implements  ITraining{
+
+    @Id @GeneratedValue
+    @Column(name="idTraining",nullable = false)
     private Integer idTraining;
+    @Column(name="length",nullable = false)
     public int length;
-    public String name;
+    @Column(name="name",nullable = false)
+    protected String name;
+    @Column(name="explanation",nullable = false)
     public String explanation;
+    @Column(name="sessionUsers")
     public List<IBodyPart> bodyPart;
     public ATraining()
     {
