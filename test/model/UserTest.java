b6a60dce3821cdf5e983d974045f3b3caed0edf9
kkuga <k.kugasanth@gmail.com>
2015-03-03 20:37:40 +0100

    Correction de UserTest.java

--- test/model/UserTest.java
+++ test/model/UserTest.java
@@ -1,6 +1,7 @@
 package model;
 
 import controller.LoginServlet;
+import controller.RegisterServlet;
 import org.hibernate.Session;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -13,13 +14,14 @@ import static org.testng.Assert.*;
 
 public class UserTest {
     User user;
-
+    User userNoDb;
 
     @BeforeMethod
     public void setUp() throws Exception {
 
         Session session = HibernateUtil.getSessionFactory().openSession();
-        user= (User)session.get(User.class,1);
+        user= (User)session.get(User.class,2);
+        userNoDb = new User("test","test@gmail.com","test");
     }
 
     @AfterMethod
@@ -29,36 +31,37 @@ public class UserTest {
     @Test
     public void testUser(){
         User user = new User("user","email","pass");
-        assertEquals("Yoshi",user.getUsername());
+        assertEquals("user",user.getUsername());
         testGetEmail();
         testValidatePassword();
     }
 
     @Test
     public void testGetUsername(){
-        assertEquals("Yoshi",user.getUsername());
+        assertEquals("kuga",user.getUsername());
     }
 
     @Test
     public void testGetEmail(){
-        assertEquals("yoshi@gmail.com",user.getEmail());
+        assertEquals("kuga@gmail.com",user.getEmail());
     }
 
     @Test
     public void testValidatePassword(){
         assertFalse(user.validatePassword("wrongpass"));
-        assertTrue(user.validatePassword("yoshi"));
+        assertTrue(user.validatePassword("kuga"));
     }
     
     @Test 
     public void testRegisterUser(){
-        assertTrue(RegisterService.register(user));      
+        //assertTrue(RegisterService.register(user));
+        assertTrue(RegisterServlet.register(userNoDb.getUsername(),userNoDb.getEmail(),userNoDb.getPassword(),userNoDb.getEmail(),"175","50"));
     }
     
     @Test
     public void testLoggedUser(){
 
-       assertTrue(LoginServlet.login(user.getUsername(),"yoshi"));
+       assertTrue(LoginServlet.login(user.getUsername(),"kuga"));
         //RegisterService.register(user);
         //assertTrue(LoginService.authenticate(user.getUsername(),user.getPassword()));
     }
