4403f852ce4a808d288ebbbf717f142a613ef503
ChristianHiroz <twinkie.hiroz@gmail.com>
2015-03-05 00:35:36 +0100

    Merge

--- src/console/CoreConsole.java
+++ src/console/CoreConsole.java
@@ -14,15 +14,15 @@ import java.util.Scanner;
 public class CoreConsole {
     public static void getConnectedHeader(AUser user){
         Util.clearConsole();
-        System.out.println("################### \n");
+        System.out.println("#################### \n");
         System.out.println("Bonjour " + user.getUsername() + ", bienvenue sur GymTonik ! \n");
-        System.out.println("################### \n");
+        System.out.println("#################### \n");
     }
 
     public static void getAnonymousHeader(){
         Util.clearConsole();
-        System.out.println("################### \n");
+        System.out.println("#################### \n");
         System.out.println("Bonjour, bienvenue sur GymTonik. 1 - Connection 2 - Inscription\n");
-        System.out.println("################### \n");
+        System.out.println("#################### \n");
     }
 }
