7056e5a874850886d189eb3e685d6eb1c0dd39c9
kkuga <k.kugasanth@gmail.com>
2015-03-05 15:21:13 +0100

    Création de HistoriqueService

---
+++ src/service/HistoriqueService.java
@@ -0,0 +1,62 @@
+package service;
+
+import model.AUser;
+import model.Exercise;
+import model.SessionUser;
+import org.hibernate.*;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
+import java.util.List;
+
+/**
+ * Created by admin on 05/03/2015.
+ */
+public class HistoriqueService {
+
+
+    private static final SessionFactory ourSessionFactory;
+    private static final ServiceRegistry serviceRegistry;
+
+    static {
+        try {
+            Configuration configuration = new Configuration();
+            configuration.configure();
+
+            serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
+            ourSessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    private static Session getSession() throws HibernateException {
+        return ourSessionFactory.openSession();
+    }
+
+
+    public static List<Exercise> getExercises(SessionUser sessionUser) {
+        Session session = getSession();
+        Transaction tx = null;
+
+        List<Exercise> exercises;
+        try {
+            tx = session.beginTransaction();
+            Query query = session.createQuery("from Historique e left join fetch e.idS u where u.id = :seanceId");
+            query.setParameter("seanceId", sessionUser.getIdS());
+            exercises = query.list();
+            tx.commit();
+            return exercises;
+        } catch (Exception e) {
+            if (tx != null)
+                tx.rollback();
+            e.printStackTrace();
+            return null;
+        }finally {
+            session.close();
+        }
+    }
+
+
+}
