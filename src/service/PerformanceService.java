3042749e4fa21fd4f30a18193083343b516f51c4
driss78210 <drisskaaouas@gmail.com>
2015-03-25 12:34:36 +0100

    Correction de la fonction getperffromsessionid, ajustement de la requête

--- src/service/PerformanceService.java
+++ src/service/PerformanceService.java
@@ -162,22 +162,23 @@ public class PerformanceService {
         return null;
     }
 
-    public static List<ExerciceSession> getPerfFromSessionId( int sessionId){
+    public static List<ExerciceSession> getPerfFromSessionId( int sessionId) {
         Session session = getSession();
         Transaction tx = null;
         List<ExerciceSession> exerciceSessions;
         try {
             tx = session.beginTransaction();
-            Query query = session.createQuery("Select h  from ExerciceSession h where h.sessionUser.idS="+ sessionId +" order by h.dateProgEffectue");
+            Query query = session.createQuery("Select h  from ExerciceSession h where h.sessionUser.idS=" + sessionId + " order by h.dateProgEffectue");
             exerciceSessions = query.list();
             tx.commit();
             return exerciceSessions;
-        }   catch (Exception e) {
+        } catch (Exception e) {
             if (tx != null)
                 tx.rollback();
             e.printStackTrace();
         }
         return null;
+    }
 
 
 
